e05c79f1499fb44c52d262c306fb6571feea961e
xfile6912 <57051773+xfile6912@users.noreply.github.com>
2020-03-08 01:15:33 +0900

    Create boj1967.cpp

---
+++ Tree/boj1967.cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include <queue>
+using namespace std;
+
+typedef struct node {
+	int vertex;
+	int length;
+}NODE;
+
+vector<NODE> graph[10001];
+vector<int> visited(10001);
+
+void initvisited(int n)
+{
+	int i;
+	for (i = 1; i <= n; i++)
+		visited[i] = 0;
+}
+int maxvalue=0;
+
+int dfs(int x)
+{
+	int i;
+	int max1=0;
+	int max2=0;
+	vector<int> a;
+	for (i = 0; i < graph[x].size(); i++)
+	{
+		if (visited[graph[x][i].vertex] == 0)
+		{
+			visited[graph[x][i].vertex] = 1;
+			int temp=dfs(graph[x][i].vertex)+graph[x][i].length;
+			a.push_back(temp);
+		}
+	}
+	sort(a.begin(),a.end());
+	if (a.size() > 1)
+	{
+		max1 = a[a.size() - 1];
+		max2 = a[a.size() - 2];
+	}
+	else if (a.size() == 1)
+		max1 = a[a.size() - 1];
+	if (max1 + max2  > maxvalue)
+		maxvalue = max1 + max2;
+	return max1;
+}
+
+int main(void)
+{
+	int n;
+	int v1, v2, length;
+	NODE tree;
+	scanf("%d", &n);
+	int i;
+	for (i = 1; i < n; i++)
+	{
+		scanf("%d %d %d", &v1, &v2, &length);//간선;
+		tree.vertex = v2;
+		tree.length = length;
+		graph[v1].push_back(tree);
+	}
+	initvisited(n);
+	visited[1] = 1;
+	int temp= dfs(1);
+	printf("%d", maxvalue);
+}
