fe4833134bcc36a3dbfd921fd76fa210dd542bd1
xfile6912 <57051773+xfile6912@users.noreply.github.com>
2020-03-19 20:23:29 +0900

    Create boj11279.cpp

---
+++ Stack, Queue, Deque, List/boj11279.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+
+int main(void)
+{
+	int n;
+	priority_queue<int> q;
+	scanf("%d", &n);
+	int i, order;
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &order);
+		if (order == 0)
+		{
+			if (q.empty())
+				printf("0\n");
+			else
+			{
+				printf("%d\n", q.top());
+				q.pop();
+			}
+		}
+		else
+			q.push(order);
+	}
+}
