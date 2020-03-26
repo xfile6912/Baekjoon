52f512eacaa6fe3af1ef29175b22ef94514eb577
xfile6912 <57051773+xfile6912@users.noreply.github.com>
2020-03-26 20:42:41 +0900

    Create boj1826.cpp

---
+++ Stack, Queue, Deque, List/boj1826.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+
+int main(void)
+{	
+	int n;
+
+	priority_queue<int> q;
+	int distance, fuel;
+	int cnt = 0;
+	int i;
+	scanf("%d", &n);
+	vector<vector<int> > info(n, vector<int>(2, 0));
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d", &info[i][0], &info[i][1]);
+	}
+	scanf("%d %d", &distance, &fuel);
+
+	sort(info.begin(), info.end());//거리순으로 정렬;
+	distance = distance - info[n - 1][0];
+	for (i = n-1; i > 0; i--)
+	{
+		info[i][0] = info[i][0] - info[i - 1][0];
+	}
+	int flag;
+	flag = 1;
+	for (i = 0; i < n; i++)
+	{
+		fuel = fuel - info[i][0];
+		if (fuel < 0)
+		{
+			while (fuel<0)
+			{
+				if (q.empty())//중간에 못가는경우
+				{
+					flag = 0;
+					break;
+				}
+				fuel += q.top();//기름을 넣어줌
+				q.pop();
+				cnt++;
+			}
+		}
+		if (flag == 0)
+		{
+			break;
+		}
+		q.push(info[i][1]);
+	}
+	if (flag == 0)
+		printf("-1");
+	else
+	{
+		fuel = fuel - distance;
+		if (fuel >= 0)
+			printf("%d", cnt);
+		else
+		{
+			while (fuel < 0)
+			{
+				if (q.empty())//중간에 못가는경우
+				{
+					flag = 0;
+					break;
+				}
+				fuel += q.top();//기름을 넣어줌
+				q.pop();
+				cnt++;
+			}
+			if (flag == 0)
+				printf("-1");
+			else
+				printf("%d", cnt);
+		}
+	}
+}
